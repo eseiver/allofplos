cad86fb278062bbddb8d11de28f77c24590152c9
Elizabeth Seiver <elizabeth.seiver@gmail.com>
2018-01-04 00:26:06 -0800

    adding hashing functions to corpus class

--- allofplos/corpus/corpus_class.py
+++ allofplos/corpus/corpus_class.py
@@ -1,12 +1,17 @@
 from collections import OrderedDict
+import hashlib
+import json
 from pathlib import Path
 import os
 import random
+from tqdm import tqdm
 
 from .. import get_corpus_dir, Article
 from ..transformations import filename_to_doi, doi_to_path
 from .gdrive import (get_zip_metadata, download_file_from_google_drive, unzip_articles,
                     zip_id, local_zip)
+
+hash_json = 'corpus_hash.json'
 
 
 class Corpus():
@@ -88,6 +93,45 @@ class Corpus():
         :param count: specify how many DOIs are to be returned
         :return: a list of random DOIs for analysis
         """
+
+        return random.sample(self.dois, count)
+
+    def hashtable(self, directory=directory):
+        """For every XML file in the corpus, create a dict of its DOI to its filehash.
+
+        :return: dict of article DOIs to hashes
+        """
+        hash_dict = {}
+        fnames = [os.path.join(directory, fname) for fname in self.files]
+        BLOCKSIZE = 65536
+        hasher = hashlib.sha256()
+        for fname in tqdm(fnames):
+            with open(fname, 'rb') as afile:
+                buf = afile.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile.read(BLOCKSIZE)
+            hash_dict[filename_to_doi(fname)] = hasher.hexdigest()
+
+        return OrderedDict(hash_dict)
+
+    def hashtable_to_json(self, hashtable=None):
+        """Dump `self.hashtable` OrderedDict into a .json file."""
+        if hashtable is None:
+            hashtable = self.hashtable()
+        with open(hash_json, 'w') as fp:
+            json.dump(hashtable, fp)
+
+    def read_hashtable(self):
+        """Read the `hash_json` file into memory.
+
+        Also see `self.hashtable{}`
+        """
+        if os.path.isfile(hash_json):
+            return OrderedDict(json.load(open(hash_json)))
+        else:
+            print("Hashtable not found; create with Corpus.hashtable()")
+            return None
 
         return random.sample(self.dois, count)
 
