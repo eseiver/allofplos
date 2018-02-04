3a22bfea219d7bcc9de7d39d6a2593c9fbb706d2
Elizabeth Seiver <elizabeth.seiver@gmail.com>
2018-02-03 16:39:48 -0800

    flesh out author and editor

--- allofplos/contributor_class.py
+++ allofplos/contributor_class.py
@@ -192,8 +192,9 @@ class Author(Contributor):
 
     Inherits from Contributor class.
     """
-    def __init__():
-        author_keys = ['contrib_initials',
+    def __init__(self, contrib_element):
+        Contributor.__init__(self, contrib_element, contrib_type='author')
+        author_keys = ['initials',
                         'given_names',
                         'surname',
                         'group_name',
@@ -204,8 +205,60 @@ class Author(Contributor):
                         'email',
                         'affiliations',
                         'author_roles',
-                        'footnotes'
+                        'footnotes',
                         ]
+        self.author_type = None
+        self.get_author_type()
+        self.credit_dict = None
+        self.get_credit_taxonomy()
+
+    def get_author_type(self):
+        """Get the type of author for a single contributor from their accompanying <contrib> element.
+        Authors can be 'corresponding' or 'contributing'. Depending on the paper, some elements have a 
+        top-level "corresp" attribute that equal yes; otherwise, corresponding status can be inferred
+        from the existence of the <xref> attribute ref-type="corresp"
+        :param contrib_element: An article XML element with the tag <contrib>
+        :return: author type (corresponding, contributing, None)
+        """
+        answer_dict = {
+            "yes": "corresponding",
+            "no": "contributing"
+        }
+
+        author_type = None
+        if self.contrib_element.get('contrib-type', None) == 'author':
+            corr = self.contrib_element.get('corresp', None)
+            if corr:
+                author_type = answer_dict.get(corr, None)
+            else:
+                temp = self.contrib_element.get('corresp', None)
+                if temp:
+                    author_type = answer_dict.get("yes", None)
+                else:
+                    author_type = answer_dict.get("no", None)
+
+        self.author_type = author_type
+
+    def get_credit_taxonomy(self):
+        """Get the contributor roles from the CREDiT taxonomy element when it is present.
+        Right now, this is is equivalent to author roles.
+        For more information about this data structure, see http://dictionary.casrai.org/Contributor_Roles
+        :param contrib_element: An article XML element with the tag <contrib>
+        :return: dictionary of contributor roles for an individual contributor
+
+        """
+        credit_dict = {}
+        for item in self.contrib_element.getchildren():
+            if item.tag == 'role':
+                content_type = item.attrib.get('content-type', None)
+                if content_type == 'http://credit.casrai.org/':
+                    content_type = 'CASRAI CREDiT taxonomy'
+                role = item.text
+                if not credit_dict.get(content_type, None):
+                    credit_dict[content_type] = [role]
+                else:
+                    credit_dict[content_type].append(role)
+        self.credit_dict = credit_dict
 
 
 class Editor(Contributor):
@@ -213,16 +266,17 @@ class Editor(Contributor):
 
     Inherits from Contributor class.
     """
-    def __init__():
-        edtior_keys = ['contrib_initials',
-                       'given_names',
-                       'surname',
-                        'ids',
-                        'rid_dict',
-                        'contrib_type',
-                        'editor_type',
-                        'email',
-                        'affiliations',
-                        'footnotes'
-                        ]
+    def __init__(self, contrib_element):
+        Contributor.__init__(self, contrib_element, contrib_type='editor')
+        self.editor_keys = ['initials',
+                           'given_names',
+                           'surname',
+                           'ids',
+                           'rid_dict',
+                           'contrib_type',
+                           'editor_type',
+                           'email',
+                           'affiliations',
+                           'footnotes',
+                           ]
 
