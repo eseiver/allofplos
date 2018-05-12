223914917d9c74927114f20f86695e2a24c188be
Elizabeth Seiver <elizabeth.seiver@gmail.com>
2018-05-11 17:50:18 -0700

    WIP: email_matching
    uses pandas and needs to use numpy instead

--- allofplos/contributor/email_matching.py
+++ allofplos/contributor/email_matching.py
@@ -1,3 +1,9 @@
+import difflib
+import re
+
+import pandas as pd
+from unidecode import unidecode
+
 
 def match_single_email(corr_author, email_dict):
     """When an aricle has a single author, match their rid to their email(s)."""
@@ -16,38 +22,87 @@ def match_single_email(corr_author, email_dict):
     return matching
 
 
+def match_to_emails(authors, email_dict):
+    """Match the email dictionary emails to each of the authors.
+    Follows a set of rules to favor matches for authors that are marked as corresponding.
+    Every author is also checked for matching on their initials, the string of their names vs the
+    string of the email address. Extra points given for 3+ length initials, when corresponding
+    author count matches the length of the email dictionary, and for full name matches.
+    The author with the highest score for each email address is assigned that email address.
+    """
+    corr_author_list = [auth for auth in authors if auth.author_type == 'corresponding']
 
-def match_to_emails(authors):
-    corr_author_list = [contrib for contrib in contrib_dict_list if contrib.get('author_type', None) == 'corresponding']
-    if not corr_author_list and email_dict:
-        print('Email but no corresponding author found for {}'.format(self.doi))
-        # matching_error = True
-    if corr_author_list and not email_dict:
-        print('Corr emails not found for {}'.format(self.doi))
-        matching_error = True
-    if len(corr_author_list) == 1:
-        matching_error = match_single_email(corr_author_list)
-    elif email_dict and len(corr_author_list) > 1 and len(set([tuple(x) for x in email_dict.values()])) > 1:
-        corr_author_list, matching_error = match_contribs_to_dicts(corr_author_list,
-                                                                   email_dict,
-                                                                   contrib_key='email')
-    elif len(corr_author_list) > 1:
-        if email_dict and (len(email_dict) == 1 or len(set([tuple(x) for x in email_dict.values()])) == 1):
-            # if there's only one email address, use it for all corr authors
-            for corr_author in corr_author_list:
-                corr_author['email'] = list(email_dict.values())[0]
-        else:
-            matching_error = True
+    # simplest scenario: 1 corresponding author, 1 email (80% of articles)
+    if len(corr_author_list) == len(email_dict) == 1:
+        matching_error = match_single_email(corr_author_list, email_dict)
+
+    # all other email scenarios
     else:
-        corr_author_list = []
+        author_dict = {auth: (auth.author_type, auth.name.get('initials', '')) for auth in authors}
+        match_tuples = []
+        for k, v in email_dict.items():
+            for key, value in author_dict.items():
+                score = 0
+                # add score if corresponding author
+                if value[0] == 'corresponding':
+                    # matters more if the number of emails matches number of corr authors
+                    if len(corr_author_list) == len(email_dict):
+                        score += 20
+                    else:
+                        print('author number mismatch for {}'.format(value))
+                        score += 3
 
-    match_error_printed = False
-    if email_dict and len(email_dict) > len(corr_author_list) > 0:
-            print('Contributing author email included for {}'
-                  .format(self.doi))
-            match_error_printed = True
-    elif email_dict and 1 < len(email_dict) < len(corr_author_list):
-        print('{} corresponding author email(s) missing for {}'
-              .format(len(corr_author_list) - len(email_dict), self.doi))
-        match_error_printed = True
+                # match initials
+                if k == value[1]:
+                    # if it's three or more characters, it's a very good initials match
+                    # ... only if there's one email address associated with it
+                    if len(k) > 2 and len(v) == 1:
+                        score += 20
+                    else:
+                        score += 10
+                # handle initials case where middle initial missing, and each initials is at least 2 characters
+                elif len(k) != len(value[1]) and len(k) >= 2 and len(value[1]) >= 2:
+                    new_k = ''.join([k[0], k[-1]])
+                    new_value = ''.join([value[1][0], value[1][-1]])
+                    if new_k == new_value:
+                        score += 5
 
+                # name to email address matching
+                # make a separate match for each email address in email_dict
+                # make email beginning into same kind of string (i.e., no non-alpha characters)
+                name_list = unidecode(key.name.get('given_names', '')).lower().split() + unidecode(key.name.get('surname', '')).lower().split()
+                for email in v:
+                    seq_2 = unidecode(email.lower().split('@')[0])
+                    regex = re.compile('[^a-zA-Z]')
+                    seq_2 = regex.sub('', seq_2)
+
+                    # add match score for each name in author's name, with extra weight for a full name match
+                    for name in name_list:
+                        matcher = difflib.SequenceMatcher(a=name, b=seq_2)
+                        partial_match = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
+                        partial_score = partial_match[-1]
+                        # give a full name match a serious boost
+                        if partial_score == len(name):
+                            score += partial_score + 10
+                        else:
+                            score += partial_score
+                    match_tuple = (k, email, key.namestrings[0], value, score)
+                    match_tuples.append(match_tuple)
+        # this needs to be replaced to use numpy instead of pandas
+        # need to have a tiebreaker
+        try:
+            columns = ['initials', 'email', 'name', 'author_info', 'match_score']
+            df = pd.DataFrame(match_tuples, columns=columns)
+            pivot = df.pivot(index='name', columns='email', values='match_score')
+            email_matches = pivot.idxmax().to_dict()
+            for k, v in email_matches.items():
+                for auth in authors:
+                    if auth.namestring == v:
+                        auth.email.append(k)
+                        break
+            # flag DOIs for match scores that are below 20
+            # what to do in this case? see if email matches initials? exclude middle name/initial from namestring?
+            # if max(pivot.max().to_dict().values()) < 20:
+            #     print(art.doi, pivot)
+        except ValueError:
+            print("Pandas error", match_tuples)
